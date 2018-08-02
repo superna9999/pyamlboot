e5ff17e1f368063c7ffd820f446b4819427440d3
Neil Armstrong <narmstrong@baylibre.com>
2018-08-02 17:12:16 +0200

    Update boot.cmd to boot from memory

--- boot.cmd
+++ boot.cmd
@@ -1,9 +1,3 @@
 echo ##### USB Boot script !! #####
-setenv autoload no
-dhcp
-setenv serverip 10.1.2.12
 setenv bootargs console=ttyAML0,115200 earlycon
-tftpboot $kernel_addr_r amlogic/Image
-tftpboot $fdt_addr_r $fdtfile
-tftpboot $ramdisk_addr_r amlogic/rootfs.cpio.uboot
-booti $kernel_addr_r $ramdisk_addr_r $fdt_addr_r
+booti 0x20080000 0x26000000 0x20000000
