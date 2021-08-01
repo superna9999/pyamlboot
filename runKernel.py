04311199b53274c3c6b79bfc726110f26f8004b9
Thomas Hebb <tommyhebb@gmail.com>
2021-08-01 14:11:33 -0700

    runKernel: migrate to argparse
    This gives us much better help text, ahead-of-time validation that all
    given files exist and are readable, and the capability to omit ramdisk
    and cmdline if we don't care about them. No existing invocations should
    be broken by this migration.

--- runKernel.py
+++ runKernel.py
@@ -6,39 +6,52 @@
 # Amlogic u-boot flashed on a device
 #
 
+import argparse
+
 from pyamlboot import pyamlboot
-import sys
 
 UBOOT_IMAGEADDR = 0x8080000
 UBOOT_DTBADDR = 0x8008000
 UBOOT_INITRDADDR = 0x13000000
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("Usage: %s <uImage path> <dtb path> <rootfs path> \"<cmdline>\"" % sys.argv[0])
-        sys.exit(1)
+    def FileNoStdin(arg):
+        if arg == "-":
+            return None
+        else:
+            return argparse.FileType('rb')(arg)
+
+    parser = argparse.ArgumentParser(
+        description="Boot a kernel via Amlogic U-Boot's USB loader mode.")
+    parser.add_argument(
+        "kernel", type=FileNoStdin,
+        help="kernel image to boot, in uImage format")
+    parser.add_argument(
+        "dtb", type=FileNoStdin,
+        help="device tree blob to pass to kernel")
+    parser.add_argument(
+        "ramdisk", type=FileNoStdin, nargs='?',
+        help="ramdisk to pass to the kernel")
+    parser.add_argument(
+        "cmdline", nargs='?', default="",
+        help="command line arguments to pass to the kernel")
+
+    args = parser.parse_args()
 
     dev = pyamlboot.AmlogicSoC()
 
-    print("Writing uImage...")
-    with open(sys.argv[1], "rb") as f:
-            b = f.read()
-    dev.writeLargeMemory(UBOOT_IMAGEADDR, b, 512, True)
+    print("Writing kernel...")
+    dev.writeLargeMemory(UBOOT_IMAGEADDR, args.kernel.read(), 512, True)
 
     print("Writing dtb...")
-    with open(sys.argv[2], "rb") as f:
-            b = f.read()
-    dev.writeLargeMemory(UBOOT_DTBADDR, b, 512, True)
+    dev.writeLargeMemory(UBOOT_DTBADDR, args.dtb.read(), 512, True)
 
-    if sys.argv[3] != "-":
-	    print("Writing rootfs...")
-	    with open(sys.argv[3], "rb") as f:
-		    b = f.read()
-	    dev.writeLargeMemory(UBOOT_INITRDADDR, b, 512, True)
+    if args.ramdisk is not None:
+        print("Writing ramdisk...")
+        dev.writeLargeMemory(UBOOT_INITRDADDR, args.ramdisk.read(), 512, True)
 
     print("Running bootm...")
-    if sys.argv[3] != "-":
-        dev.tplCommand(1, "setenv bootargs %s ; bootm 0x%x 0x%x 0x%x" % (sys.argv[4], UBOOT_IMAGEADDR, UBOOT_INITRDADDR, UBOOT_DTBADDR))
+    if args.ramdisk is not None:
+        dev.tplCommand(1, "setenv bootargs %s ; bootm 0x%x 0x%x 0x%x" % (args.cmdline, UBOOT_IMAGEADDR, UBOOT_INITRDADDR, UBOOT_DTBADDR))
     else:
-        dev.tplCommand(1, "setenv bootargs %s ; bootm 0x%x - 0x%x" % (sys.argv[4], UBOOT_IMAGEADDR, UBOOT_DTBADDR))
-
+        dev.tplCommand(1, "setenv bootargs %s ; bootm 0x%x - 0x%x" % (args.cmdline, UBOOT_IMAGEADDR, UBOOT_DTBADDR))
