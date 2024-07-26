85c1fcc9d2c4f1f23164a52f3ecea3211937f2a0
Vladimir Mitrofanov <vvmitrofanov@salutedevices.com>
2024-07-26 14:57:33 +0300

    pyamlboot: change amlogic SoC detection procedure
    Device detection procedure was modified to wait the response
    with amlogic identity. In case when device is starting from
    DDR copy of uboot the device appears on the bus earlier than
    it enters to flashing mode. When we send requests for flash
    during switch interval, we get wrong responses, that's why
    we need to wait until device will be fully initialized.
    
    Signed-off-by: Vladimir Mitrofanov <vvmitrofanov@salutedevices.com>
    Change-Id: Id6920d62deb4753e64eb25ede801b679e6cf46ef

--- pyamlboot/pyamlboot.py
+++ pyamlboot/pyamlboot.py
@@ -11,7 +11,6 @@ Amlogic USB Boot Protocol Library
 import string
 import os
 import usb.core
-import usb.util
 from struct import Struct, unpack, pack
 
 REQ_WRITE_MEM = 0x01
@@ -75,6 +74,13 @@ class AmlogicSoC(object):
                                wValue = address >> 16,
                                wIndex = address & 0xffff,
                                data_or_wLength = data)
+
+    def disposeDevice(self):
+        try:
+            usb.util.dispose_resources(self.dev)
+            self.dev = None
+        except Exception as e:
+            print("Can't release device. {0}: {1}".format(type(e).__name__, e))
 
     def writeMemory(self, address, data):
         """Write some data to memory"""
